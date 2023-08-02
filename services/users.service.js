c4086f671c49fd2b0bc8159a7af0e8013d683454
mkokor <matijakokorr@gmail.com>
2023-08-02 21:07:59 +0200

    user response dto changed

--- services/users.service.js
+++ services/users.service.js
@@ -1,8 +1,14 @@
 const { User } = require("../models/User");
 
 const getAllUsers = async () => {
-  const allUsers = await User.find();
-  return allUsers;
+  const allUsers = await User.find().populate("roles");
+  return allUsers.map((user) => ({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    username: user.username,
+    email: user.email,
+    roles: user.roles.map((role) => role.name),
+  }));
 };
 
 module.exports = {
